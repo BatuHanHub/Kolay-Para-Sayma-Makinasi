5d311452a2ae4e367a51ebd7406ff53f8e2b3c25
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2022-10-12 23:20:27 +0300

    Update Kolay Para Sayma Makinası.py

--- Kolay Para Sayma Makinası.py
+++ Kolay Para Sayma Makinası.py
@@ -1,96 +1,82 @@
-print("""Bakkal Hesap Makinası, version 3.0 -release (x86_x64)
+print("""Kolay Para Sayma Makinası, version 1.0 -release (x86_x64)
 Licence GPLv3+ : GNU GPL version 3 <https://www.gnu.org/licenses/gpl-3.0.html>
 copyright (C) 2022 BatuHanHub Software 
 
 This is free software; you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.""")
 
+print("Kolay Para Sayma Programına Hoşgeldiniz!\n")
+
 while True:
 
-##########################################################################################################
+    ikiYuz=int(input("kaç tane 200tl niz var:"))
+    
+    if ikiYuz > 0:
+        pass
+    elif ikiYuz < 0:
+        print("\nhata:lütfen sayı giriniz\n")
+        continue
+    
+    yuz=int(input("kaç tane 100tl niz var:"))
+    
+    if yuz > 0:
+        pass
+    elif yuz < 0:
+        print("\nhata:lütfen sayı giriniz\n")
+        continue
+    
+    elli=int(input("kaç tane 50tl niz var:"))
+    
+    if elli > 0:
+        pass
+    elif elli < 0:
+        print("\nhata:lütfen sayı giriniz\n")
+        continue
+    
+    yirmi=int(input("kaç tane 20tl niz var:"))
+    
+    if yirmi > 0:
+        pass
+    elif yirmi < 0:
+        print("\nhata:lütfen sayı giriniz\n")
+        continue
+    
+    on=int(input("kaç tane 10tl niz var:"))
+    
+    if on > 0:
+        pass
+    elif on < 0:
+        print("\nhata:lütfen sayı giriniz\n")
+        continue
+    
+    bes=int(input("kaç tane 5tl niz var:"))
+    
+    if bes > 0:
+        pass
+    elif bes < 0:
+        print("\nhata:lütfen sayı giriniz\n")
+        continue
+    
+    bir=int(input("kaç tane 1tl niz var:"))
+    
+    if bir > 0:
+        pass
+    elif bir < 0:
+        print("\nhata:lütfen sayı giriniz\n")
+        continue
 
-#koli miktarı
+    islem1= ikiYuz * 200
+    islem2= yuz * 100
+    islem3= elli * 50
+    islem4= yirmi * 20
+    islem5= on * 10
+    islem6= bes * 5
+    islem7= bir * 1
 
- Box = int(input("\nKac tane koliniz var? : ")) #kullanıcıdan koli miktarını istiyor.
- 
- if Box > 0:
-   pass
-   
- elif Box < 0:
-   print("\nhatalı sayı gidiniz lütfen düzeltiniz\n")  
-   continue 
+    sonuc1= islem1 + islem2 + islem3 + islem4 + islem5 + islem6 + islem7
 
-#########################################################################################################
+    print("toplam paranız:", sonuc1)
 
-#biri koli içindeki su
+    print("\ndiğer projelerim için: <https://github.com/BatuHanHub>")
 
- BoxWater = int(input("bir kolide kaç tane su var? : ")) #1 kolideki su miktarını istiyor
- 
- if BoxWater > 0:
-   pass
-   
- elif BoxWater < 0:
-   print("\nhatalı sayı gidiniz lütfen düzeltiniz\n")
-   continue
-
-#bütün kolideki su hesaplanıyor
-
- TotalWater = Box * BoxWater #işlem1:toplam koli sayısı ile 1 kolide olan su miktarını çarpıyor.
- print("*toplam kolilerdeki su miktarı :",TotalWater)
-
-##########################################################################################################
-
-#alışfiyatı
-
- PurchasePrice = float(input("Alısfiyatınızı giriniz: ")) #suyun alış fiyatını soruyor.
- 
- if PurchasePrice > 0:
-   pass
-   
- elif PurchasePrice < 0:
-   print("\nhatalı sayı gidiniz lütfen düzeltiniz örneğin x.x yapınız\n")   
-   continue
-
- Water = PurchasePrice / TotalWater
- print("*bir suyun alısfiyatı:", Water)
-
-###########################################################################################################
-
-#Kâr marjı
-
- Gain = float(input("kâr marjınızı giriniz: ")) #sudan kazanılan parayı soruyor
-
- if Gain > 0:
-   pass
-   
- elif Gain < 0:
-   print("\nhatalı sayı gidiniz lütfen düzeltiniz örneğin x.x yapınız\n")
-   continue
-
-###########################################################################################################
- 
-#suyun toplam satış fiyatı
- print("\n SONUCLAR")
-
- Total = Water + Gain
-
- print("\nsatıs fiyatınız: ", Total)
-
-#toplam kolilerdeki su fiyatı
-
- Process1 = Total * TotalWater
-
- print("Toplam kolideki su fiyatınız: ", Process1)
-
-#toplam su fiyatı ile Kâr hesaplanıyor
-
- TotalGain = TotalWater * Gain
-
- print("toplam sudan kazandıgınız kâr: ", TotalGain)
-
-
-############################################################################################################
-
-print("\ndiğer projelerim için: <https://github.com/BatuHanHub>")
-
-input("\nbir tusa basarak pencereyi kapatabilirsiniz...") #ekranın hemen kapanmasını engelliyor.
+    input("Cıkmak icin bir tusa basınız...")
