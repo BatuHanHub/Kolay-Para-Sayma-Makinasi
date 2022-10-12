4f054d15e260b82ccb221e4e2cd0fe2c365c79ea
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2022-10-12 11:03:27 +0300

    Update Kolay Para Sayma Makinası.py

--- Kolay Para Sayma Makinası.py
+++ Kolay Para Sayma Makinası.py
@@ -7,27 +7,33 @@ There is NO WARRANTY, to the extent permitted by law.""")
 
 print("Kolay Para Sayma Programına Hoşgeldiniz!\n")
 
-ikiYuz=int(input("kaç tane 200tl niz var:"))
-yuz=int(input("kaç tane 100tl niz var:"))
-elli=int(input("kaç tane 50tl niz var:"))
-yirmi=int(input("kaç tane 20tl niz var:"))
-on=int(input("kaç tane 10tl niz var:"))
-bes=int(input("kaç tane 5tl niz var:"))
-bir=int(input("kaç tane 1tl niz var:"))
+while True:
 
-islem1= ikiYuz * 200
-islem2= yuz * 100
-islem3= elli * 50
-islem4= yirmi * 20
-islem5= on * 10
-islem6= bes * 5
-islem7= bir * 1
+ ikiYuz=int(input("kaç tane 200tl niz var:"))
+ yuz=int(input("kaç tane 100tl niz var:"))
+ elli=int(input("kaç tane 50tl niz var:"))
+ yirmi=int(input("kaç tane 20tl niz var:"))
+ on=int(input("kaç tane 10tl niz var:"))
+ bes=int(input("kaç tane 5tl niz var:"))
+ bir=int(input("kaç tane 1tl niz var:"))
 
-sonuc1= islem1 + islem2 + islem3 + islem4 + islem5 + islem6 + islem7
+ islem1= ikiYuz * 200
+ islem2= yuz * 100
+ islem3= elli * 50
+ islem4= yirmi * 20
+ islem5= on * 10
+ islem6= bes * 5
+ islem7= bir * 1
 
-print("toplam paranız:", sonuc1)
 
-print("\ndiğer projelerim için: <https://github.com/BatuHanHub>")
+ sonuc1= islem1 + islem2 + islem3 + islem4 + islem5 + islem6 + islem7
 
-input("Cıkmak icin bir tusa basınız...")
-  
+ if sonuc1 > 0:
+   print("toplam paranız:", sonuc1)
+   break
+ 
+ else:
+   print("\nHata:lütfen sayı yazınız\n")
+
+print("\ndiğer projelerim için: <https://github.com/BatuHanHub>")  
+input("\nÇıkmak için bir tuşa basınız...")
